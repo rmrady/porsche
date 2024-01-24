82e3cb3f05907de2845556ce2ca1aa7792575beb
razieh7 <rmrady758@gmail.com>
2024-01-24 16:25:51 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Headers from './component/header'
+import Section from './component/section'
+import Footer from './component/footer'
+
+function App() {
+  return (
+    <main>
+        <Headers/>
+        <Section/>
+        <Footer/>
+    </main>
+  )
+}
+
+export default App
