db9dfad8aacd93c4af3d9a5fb04cd9422e004734
razieh7 <rmrady758@gmail.com>
2024-01-24 17:50:54 +0330

    secton-one

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import macan from '../assets/img/Macan.avif'
 
 export default function Headers() {
-  return (
-    <header>
-
-    </header>
-  )
+    return (
+        <header>
+            <div className='w-full h-[100vh]'>
+                <img className='w-full h-[100vh]' src={macan} alt="" />
+            </div>
+            <div className='w-[100%] absolute top-0 text-white pt-10 px-20'>
+                <div className='flex justify-between'>
+                    <div className='flex'>
+                        <i class="bi bi-list"></i>
+                        <p>Menu</p>
+                    </div>
+                    <p className='text-[30px]'>PORSCHE</p>
+                    <i className="bi bi-person text-[20px]"></i>
+                </div>
+            </div>
+            <div className='text-[90px] text-white absolute bottom-52 px-32'>01/25/2024</div>
+            <button className=' absolute bottom-40  text-white border border-white px-6 py-3 mx-32'>Electrify Now</button>
+        </header>
+    )
 }
--- src/component/section.jsx
+++ src/component/section.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import imge1 from '../assets/img/imge1.avif'
+import imge2 from '../assets/img/imge2.avif'
+import imge3 from '../assets/img/imge3.avif'
 
 export default  function Section() {
   return (
     <section>
+        <div className='w-[100%] flex justify-center mt-20 '>
+            <div className='w-[30%] mr-[-80px]'><img className='w-[70%] rounded-xl' src={imge1} alt="" /></div>
+            <div className='w-[30%] mr-[-80px]'><img className='w-[70%] rounded-xl' src={imge2} alt="" /></div>
+            <div className='w-[30%] mr-[-80px]'><img className='w-[70%] rounded-xl' src={imge3} alt="" /></div>
+
+        </div>
 
     </section>
   )
